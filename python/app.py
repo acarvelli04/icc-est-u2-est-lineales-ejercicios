2d5742c30ba56ee374a3ee1312ad8916a8e944de
ArielCalleSteven <a.calleduma123@gmail.com>
2025-12-15 12:55:16 -0500

    validaciones parentesis

--- python/app.py
+++ python/app.py
@@ -1,7 +1,13 @@
 from sign_validator import SignValidator
-def run_sing_validator¨(
-    singn
-)
 
-if __name__ = "__main__":
-    run_sing_validator()
+def run_sign_validator():
+    validator = SignValidator()
+    
+    cadena1 = "{[()]}"  # Debería ser True
+    cadena2 = "{[(])}"  # Debería ser False 
+    
+    print(f"Cadena: {cadena1} es valida? {validator.is_valid(cadena1)}")
+    print(f"Cadena: {cadena2} es valida? {validator.is_valid(cadena2)}")
+
+if __name__ == "__main__":
+    run_sign_validator()
--- python/sign_validator.py
+++ python/sign_validator.py
@@ -1,13 +1,26 @@
 class SignValidator:
-
+    
     def is_valid(self, text: str) -> bool:
         stack = []
-
+        
         for char in text:
+            
             if char == '(' or char == '{' or char == '[':
                 stack.append(char)
+            
             elif char == ')' or char == '}' or char == ']':
                 if not stack:
                     return False
-                # aunque pase si no esta vacio
-                # 
+                
+                ultimo = stack.pop()
+                
+                
+                if char == ')' and ultimo != '(':
+                    return False
+                if char == '}' and ultimo != '{':
+                    return False
+                if char == ']' and ultimo != '[':
+                    return False
+        
+       
+        return len(stack) == 0
